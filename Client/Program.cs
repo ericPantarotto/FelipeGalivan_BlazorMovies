62b6ef0454fb4da0c363b1b6723b347e72b068ce
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-02-20 16:29:43 +0100

    DI and DI with Interface

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorMovies.Client;
+using BlazorMovies.Client.Helpers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +9,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+ConfigureServices(builder.Services);
+
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services)
+{
+    services.AddSingleton<SingletonService>();
+    services.AddTransient<TransientService>();
+    services.AddTransient<IRepository, RepositoryInMemory>();
+}
