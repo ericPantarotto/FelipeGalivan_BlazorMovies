bf0b28de609d64235f9d13931ac76fd84e4d61f4
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-02-20 21:46:17 +0100

    Partial Class

---
+++ Client/Pages/Counter.razor.cs
@@ -0,0 +1,58 @@
+﻿using BlazorMovies.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+//using Microsoft.JSInterop;
+
+namespace BlazorMovies.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] SingletonService singleton { get; set; }
+        [Inject] TransientService transient { get; set; }
+        //[Inject] IJSRuntime js { get; set; }
+        //[CascadingParameter] public AppState AppState { get; set; }
+
+        private int currentCount = 0;
+        //private static int currentCountStatic = 0;
+        //IJSObjectReference module;
+
+        private List<Movie>? movies;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            movies = new();
+        }
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleton.Value += 1;
+            transient.Value += 1;
+        }
+
+        //[ JSInvokable]
+        //public async Task IncrementCount()
+        //{
+        //    module = await js.InvokeAsync<IJSObjectReference>("import", "./js/Counter.js");
+        //    await module.InvokeVoidAsync("displayAlert", "hello world");
+
+        //    currentCount++;
+        //    singleton.Value += 1;
+        //    transient.Value += 1;
+        //    currentCountStatic++;
+        //    await js.InvokeVoidAsync("dotnetStaticInvocation");
+        //}
+
+        //private async Task IncrementCountJavaScript()
+        //{
+        //    await js.InvokeVoidAsync("dotnetInstanceInvocation",
+        //        DotNetObjectReference.Create(this));
+        //}
+
+        //[JSInvokable]
+        //public static Task<int> GetCurrentCount()
+        //{
+        //    return Task.FromResult(currentCountStatic);
+        //}
+    }
+}
