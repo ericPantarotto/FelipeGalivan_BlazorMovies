f0352af8d5cdb3e543d984ae6880a7c89c38e2f1
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-02-26 00:00:45 +0100

    lazy loading

--- Client/Pages/Counter.razor.cs
+++ Client/Pages/Counter.razor.cs
@@ -1,11 +1,26 @@
-﻿namespace BlazorMovies.Client.Pages {
+﻿using MathNet.Numerics.Statistics;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorMovies.Client.Pages {
     public partial class Counter
     {
+        [Inject] IJSRuntime? JS { get; set; }
 
         private int currentCount = 0;
+        IJSObjectReference? module;
 
-        public void IncrementCount()
+        [JSInvokable]
+        public async void IncrementCount()
         {
+            var array = new double[] { 1, 2, 3, 4, 5 };
+            double maxItem = array.Maximum();
+            double minItem = array.Minimum();
+
+            module = await JS!.InvokeAsync<IJSObjectReference>("import", "./js/Counter.js");
+            await module!.InvokeVoidAsync("displayAlert", $"Max is {maxItem} and min is {minItem}");
+
+
             currentCount++;
         }
     }
