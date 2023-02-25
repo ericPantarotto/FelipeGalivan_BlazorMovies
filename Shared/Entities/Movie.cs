1a64be512d0a76ed23d55f513849619a7b3e6dab
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-02-25 15:15:19 +0100

    UI Improvement

--- Shared/Entities/Movie.cs
+++ Shared/Entities/Movie.cs
@@ -1,8 +1,21 @@
 ﻿namespace BlazorMovies.Shared.Entities
 {
-    public class Movie
-    {
+    public class Movie {
         public string Title { get; set; } = string.Empty;
         public DateTime ReleaseDate { get; set; }
+        public string Poster { get; set; } = string.Empty;
+        public string? TitleBrief { get 
+            {
+                if (string.IsNullOrEmpty(Title))
+                {
+                    return null;
+                }
+                if (Title.Length > 60)
+                {
+                    return $"{Title[60..]} ...";
+                }
+                return Title;
+            }
+        }
     }
 }
