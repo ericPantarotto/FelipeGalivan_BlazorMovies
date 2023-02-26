54f85c83d3a4049dec54d1a16d5cc49415eeaadb
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-02-26 20:43:42 +0100

    Updating Records

--- Shared/Entities/Genre.cs
+++ Shared/Entities/Genre.cs
@@ -4,8 +4,7 @@ namespace BlazorMovies.Shared.Entities {
     public class Genre
     {
         public int Id { get; set; }
-        //[Required]
-        [Required(ErrorMessage = "The field name is required for creating a new genre")]
+        [Required(ErrorMessage = "The field name is required")]
         public string Name { get; set; } = string.Empty;
         //public List<MoviesGenres> MoviesGenres { get; set; } = new List<MoviesGenres>();
     }
