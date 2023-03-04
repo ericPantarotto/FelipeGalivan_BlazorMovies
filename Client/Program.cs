2a02a326c0fe5d16429c35cc96519f47f50624ad
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-03-04 01:42:58 +0100

    Creating genre

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorMovies.Client;
 using BlazorMovies.Client.Helpers;
+using BlazorMovies.Client.Repository;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,4 +17,8 @@ await builder.Build().RunAsync();
 static void ConfigureServices(IServiceCollection services)
 {
     services.AddTransient<IRepository, RepositoryInMemory>();
+    services.AddScoped<IHttpService, HttpService>();
+    services.AddScoped<IGenreRepository, GenreRepository>();
+    //services.AddScoped<IPersonRepository, PersonRepository>();
+    //services.AddScoped<IMoviesRepository, MoviesRepository>();
 }
