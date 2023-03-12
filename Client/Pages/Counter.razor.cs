7142516cb726486fce994c2ae5d4d411f4cc2209
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-03-12 12:50:00 +0100

    Reading AuthenticationState from C#

--- Client/Pages/Counter.razor.cs
+++ Client/Pages/Counter.razor.cs
@@ -1,27 +1,26 @@
-﻿using MathNet.Numerics.Statistics;
-using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BlazorMovies.Client.Pages {
     public partial class Counter
     {
-        [Inject] IJSRuntime? JS { get; set; }
-
+        [CascadingParameter] private Task<AuthenticationState>? AuthenticationState { get; set; }
         private int currentCount = 0;
-        IJSObjectReference? module;
 
-        [JSInvokable]
-        public async void IncrementCount()
+        public async Task IncrementCount()
         {
-            var array = new double[] { 1, 2, 3, 4, 5 };
-            double maxItem = array.Maximum();
-            double minItem = array.Minimum();
+            AuthenticationState authState = await AuthenticationState!;
+            var user = authState.User;
 
-            module = await JS!.InvokeAsync<IJSObjectReference>("import", "./js/Counter.js");
-            await module!.InvokeVoidAsync("displayAlert", $"Max is {maxItem} and min is {minItem}");
+            if (user.Identity!.IsAuthenticated)
+            {
+                currentCount++;
+            }
+            else
+            {
+                currentCount--;
+            }
 
-
-            currentCount++;
         }
     }
 }
